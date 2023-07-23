6fb268b64c63d6402a36aa3358935b549158f15b
ruinedme <johndoe@example.com>
2023-07-23 14:07:40 -0500

    day14 attempt 2

--- src/day14.rs
+++ src/day14.rs
@@ -11,93 +11,91 @@ pub fn run_day14(inputs: &String) {
 
 fn day14_1(inputs: &String) -> usize {
     const MAX_STEPS: usize = 10;
-    let (mut template, rules) = parse_input(inputs);
-    println!("template: {}", template);
-    println!("rules: {:?}", rules);
-    let mut step = 0;
-    while step < MAX_STEPS {
-        template = process_step(&template, &rules);
-        step += 1;
-    }
-    let counts = get_counts(&template);
+    let (template, rules) = parse_input(inputs);
 
-    let max = counts
-        .iter()
-        .max_by(|a,b| a.1.cmp(&b.1))
-        .map(|(_k,v)| v)
-        .unwrap();
-    let min = counts
-        .iter()
-        .min_by(|a,b| a.1.cmp(&b.1))
-        .map(|(_k,v)| v)
-        .unwrap();
-    println!("min: {:?}, max: {:?}", min, max);
-    return max - min;
+    return get_min_max_difference(MAX_STEPS, &rules, &template);
 }
 
 fn day14_2(inputs: &String) -> usize {
-    const MAX_STEPS: usize = 10;
-    let (mut template, rules) = parse_input(inputs);
-    let mut step = 0;
-    while step < MAX_STEPS {
-        println!("step: {}, len: {}", step, template.len());
-        template = process_step(&template, &rules);
-        step += 1;
-    }
-    let counts = get_counts(&template);
+    const MAX_STEPS: usize = 40;
+    let (template, rules) = parse_input(inputs);
 
-    let max = counts
-        .iter()
-        .max_by(|a,b| a.1.cmp(&b.1))
-        .map(|(_k,v)| v)
-        .unwrap();
-    let min = counts
-        .iter()
-        .min_by(|a,b| a.1.cmp(&b.1))
-        .map(|(_k,v)| v)
-        .unwrap();
-    println!("min: {:?}, max: {:?}", min, max);
-    return max - min;
+    return get_min_max_difference(MAX_STEPS, &rules, &template);
 }
 
-fn parse_input(inputs: &String) -> (String, HashMap<&str,String>) {
+fn parse_input(inputs: &String) -> (String, HashMap<String,(String,String)>) {
     let mut lines = inputs.lines().into_iter();
     let polymer_template = lines.next().unwrap();
     lines.next();
-    let mut rules: HashMap<&str, String> = HashMap::new();
+    //Each rule will expand into 2 of the other rules. We add both here as a () to avoid needless string parsing later
+    let mut rules: HashMap<String, (String,String)> = HashMap::new();
 
     for line in lines {
         let split: Vec<&str> = line.split(" -> ").collect();
-        let mut v: Vec<char> = split[1].chars().collect();
-        let c = split[0].char_indices().next().unwrap();
-        v.insert(0, c.1);
-        let v = v.iter().map(|c| *c).collect::<String>();
-        rules.insert(split[0], v);
+        let k = split[0].to_string();
+        let mut v = split[1].to_string();
+        let mut v2 = v.clone();
+        
+        let k_chars: Vec<char> = k.chars().collect();
+        v.insert(0, k_chars[0]);
+        v2.push(k_chars[1]);
+        rules.insert(k, (v,v2));
     }
     return (polymer_template.to_string(),rules);
 }
 
-//WAY TO SLOW...
-fn process_step(template: &String, rules: &HashMap<&str,String>) -> String {
-    let mut new_template = String::with_capacity(template.len());
+fn get_min_max_difference(steps: usize, rules: &HashMap<String,(String, String)>, template: &String) -> usize {
+
+    let mut pair_count: HashMap<String, usize> = HashMap::with_capacity(rules.len());
+    //get inital pairs
     let mut index = 0;
-
     while index < template.len() - 1 {
-        let pair: &str = &template[index..index+2];
-        let to_insert = rules.get(pair).unwrap();
-        
-        new_template.push_str(to_insert.as_str());
-
+        let pair = &template[index..index+2];
+        *pair_count.entry(pair.to_string()).or_insert(0) += 1;
         index += 1;
     }
-    new_template.push_str(template.get(template.len() -1..).unwrap());
-    return new_template.to_string();
-}
 
-fn get_counts(template: &String) -> HashMap<char, usize> {
-    let mut counts: HashMap<char, usize> = HashMap::new();
+    //get counts for n steps of each pair
+    for _step in 1..=steps {
+        let mut new_pairs: HashMap<String,usize> = pair_count.clone();
+        //get counts of each new pair
+        pair_count.iter()
+            .for_each(|x| {
+                let pairs = rules.get(x.0).unwrap();               
 
-    template.chars().into_iter().for_each(|c| *counts.entry(c).or_insert(0) += 1);
-    println!("counts: {:?}", counts);
-    return counts;
+                //Because maps aren't ordered, the pairs are not processed in the same order each time
+                //This only evals to true if a pair has added an amount to a previous pair. 
+                //Then we subtract the previous value
+                //get rid of old pair
+                let current = new_pairs.get(&x.0.to_string()).unwrap();
+                if current >= x.1 {
+                    *new_pairs.entry(x.0.to_string()).or_default() = *current - x.1;
+                }
+
+                //add the new pairs
+                *new_pairs.entry(pairs.0.to_string()).or_insert(0) += x.1;
+                *new_pairs.entry(pairs.1.to_string()).or_insert(0) += x.1;                
+            });
+        
+        //add new pair counts to total counts
+        pair_count = new_pairs;
+    }
+    
+    //get counts of each letter, we only care about the 1st letter of each pair + the last letter of the template
+    let mut letter_count: HashMap<char, usize> = HashMap::with_capacity((rules.len() as f64).sqrt() as usize);
+    pair_count.iter()
+        .for_each(|x| {
+            let c = x.0.char_indices().next().unwrap().1;
+            *letter_count.entry(c).or_insert(0) += x.1;
+        });
+
+    //take last letter of the of the template and add 1 to letter count
+    let c = template.char_indices().nth(template.len()-1).unwrap().1;
+    *letter_count.entry(c).or_default() += 1;
+
+    //get min/max value of each letter
+    let min = letter_count.iter().min_by(|a,b| a.1.cmp(&b.1)).map(|(_k,v)| v).unwrap();
+    let max = letter_count.iter().max_by(|a,b| a.1.cmp(&b.1)).map(|(_k,v)| v).unwrap();
+
+    return max - min;
 }
