e71330010fddb0b6fd373867402364100bee7abf
ruinedme <johndoe@example.com>
2023-10-22 01:14:46 -0500

    fix formatting

--- src/day20.rs
+++ src/day20.rs
@@ -15,7 +15,10 @@ fn day20_1(inputs: &String) -> usize {
     grid = enhance_image(2, grid, enhancement_algo, image_width);
 
     // count up all the lit pixels
-    let sum = grid.iter().flatten().fold(0, |acc: usize, &x| if x == HASH { acc + 1} else { acc });
+    let sum = grid
+        .iter()
+        .flatten()
+        .fold(0, |acc: usize, &x| if x == HASH { acc + 1 } else { acc });
 
     return sum;
 }
@@ -27,7 +30,10 @@ fn day20_2(inputs: &String) -> usize {
     grid = enhance_image(50, grid, enhancement_algo, image_width);
     // count up all the lit pixels
     // given the logic in enhance_image() this fails on odd numbered iterations for my input because all pixels outside the image would be considered light
-    let sum = grid.iter().flatten().fold(0, |acc: usize, &x| if x == HASH { acc + 1} else { acc });
+    let sum = grid
+        .iter()
+        .flatten()
+        .fold(0, |acc: usize, &x| if x == HASH { acc + 1 } else { acc });
 
     return sum;
 }
@@ -48,7 +54,7 @@ fn print_grid(grid: &Vec<Vec<u8>>) {
     println!("{:=<125}", "=");
 }
 /// Returns enhancement algorithm index
-fn enhance_pixel(pixel: (usize, usize), grid: &Vec<Vec<u8>>,filler: u8) -> usize {
+fn enhance_pixel(pixel: (usize, usize), grid: &Vec<Vec<u8>>, filler: u8) -> usize {
     let x = pixel.0;
     let y = pixel.1;
     let width = grid[0].len() - 1;
@@ -86,7 +92,7 @@ fn enhance_pixel(pixel: (usize, usize), grid: &Vec<Vec<u8>>,filler: u8) -> usize
     }
 
     let pixel_matrix: [[u8; 3]; 3] = [top, mid, bot];
-    
+
     // convert matrix to index
     let mut p = 0;
     pixel_matrix.iter().flatten().for_each(|&x| {
@@ -99,12 +105,16 @@ fn enhance_pixel(pixel: (usize, usize), grid: &Vec<Vec<u8>>,filler: u8) -> usize
     return p;
 }
 
-fn enhance_image(iterations: usize, mut grid: Vec<Vec<u8>>, algo: &[u8], image_size: usize) -> Vec<Vec<u8>> {
+fn enhance_image(
+    iterations: usize,
+    mut grid: Vec<Vec<u8>>,
+    algo: &[u8],
+    image_size: usize,
+) -> Vec<Vec<u8>> {
     let mut cycles = iterations;
     let mut grid_width = grid.len();
     let mut image_width = image_size;
     while cycles > 0 {
-
         let mut t_grid = grid.clone();
         // My input was 9 dark pixels(0) = 1 light pixel
         // 9 light pixels (512) = 1 dark pixel
@@ -115,9 +125,9 @@ fn enhance_image(iterations: usize, mut grid: Vec<Vec<u8>>, algo: &[u8], image_s
         // unsure if this pattern holds true for all inputs
         let mut filler = 0; // 0 on odd
         if cycles % 2 == 0 {
-            filler = algo.len() -1; //511 on even
+            filler = algo.len() - 1; //511 on even
         }
-        
+
         // scan the whole grid
         for i in 0..grid.len() {
             for j in 0..grid[0].len() {
@@ -130,9 +140,9 @@ fn enhance_image(iterations: usize, mut grid: Vec<Vec<u8>>, algo: &[u8], image_s
         image_width += 2;
         cycles -= 1;
         // print_grid(&grid);
-        
+
         // resize the grid
-        if image_width >= grid_width -1 {
+        if image_width >= grid_width - 1 {
             grid = resize_grid(grid);
             grid_width = grid.len();
         }
@@ -142,7 +152,7 @@ fn enhance_image(iterations: usize, mut grid: Vec<Vec<u8>>, algo: &[u8], image_s
 }
 
 //TODO: rework to be similar to resize_grid
-fn create_grid(inputs: &String) -> (&[u8],Vec<Vec<u8>>,usize) {
+fn create_grid(inputs: &String) -> (&[u8], Vec<Vec<u8>>, usize) {
     let mut lines = inputs.lines();
     // parse
     let enhancement_algo = lines.next().unwrap().as_bytes();
@@ -155,10 +165,9 @@ fn create_grid(inputs: &String) -> (&[u8],Vec<Vec<u8>>,usize) {
     // create an empty row to use as padding
     let empty_row: Vec<u8> = vec![DOT; image_width * 3]; // Vec::with_capacity(line_width * 3);
 
-
     //fill out grid
     let mut grid: Vec<Vec<u8>> = Vec::with_capacity(image_width * 3);
-    
+
     // insert filler rows
     for _ in 0..image_width {
         let e = empty_row.clone();
@@ -187,19 +196,19 @@ fn create_grid(inputs: &String) -> (&[u8],Vec<Vec<u8>>,usize) {
         grid.push(e);
     }
 
-    return (enhancement_algo, grid,image_width);
+    return (enhancement_algo, grid, image_width);
 }
 
 fn resize_grid(grid: Vec<Vec<u8>>) -> Vec<Vec<u8>> {
     let grid_width = grid[0].len();
     let new_width = grid_width * 2;
-    let offset = (grid_width as f64 /2f64).ceil() as usize;
+    let offset = (grid_width as f64 / 2f64).ceil() as usize;
 
-    let mut new_grid: Vec<Vec<u8>> = vec![vec![DOT;new_width];new_width];
+    let mut new_grid: Vec<Vec<u8>> = vec![vec![DOT; new_width]; new_width];
 
     for i in 0..grid_width {
         for j in 0..grid_width {
-            new_grid[i+offset][j+offset] = grid[i][j];
+            new_grid[i + offset][j + offset] = grid[i][j];
         }
     }
 
