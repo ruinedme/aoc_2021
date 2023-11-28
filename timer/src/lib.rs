094b5d9995dac758210b11e5bf820a0b946704eb
ruinedme <johndoe@example.com>
2023-11-27 22:54:48 -0600

    add timings

--- timer/src/lib.rs
+++ timer/src/lib.rs
@@ -73,13 +73,12 @@ macro_rules! profile {
         let _start = timer::read_cpu_timer();
         $($stmts)*
         let _end = timer::read_cpu_timer();
-        let elapsed_time = ((_end - _start) * 1000000) as f64 / timer::os_freq() as f64;
-        if elapsed_time > 1000f64 {
-            println!("Elapsed Time: {:.4}ms", elapsed_time / 1000f64);
-        } else if elapsed_time < 1000000f64 {
-            println!("Elapsed Time: {:.4}s", elapsed_time / 1000000f64);
-        }else {
-            println!("Elapsed Time: {:.4}\u{03bc}s", elapsed_time);
+        let elapsed_time = (_end - _start) as f64;
+
+        if elapsed_time > 10_000_000_000.0 {
+            println!("elapsed: {:.4}s", (elapsed_time as f64 / timer::cpu_freq() as f64));    
+        } else {
+            println!("elapsed: {:.4}ms", (elapsed_time as f64 / 1_000_000.0));    
         }
     };
 }
