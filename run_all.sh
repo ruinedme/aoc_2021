ed005096e73cb05d40016f0d295a8db1a9c744ae
ruinedme <johndoe@example.com>
2023-11-28 10:58:26 -0600

    add util script

---
+++ run_all.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+./target/release/aoc_2021.exe 1 ./resources/day1_input.txt
+./target/release/aoc_2021.exe 2 ./resources/day2_input.txt
+./target/release/aoc_2021.exe 3 ./resources/day3_input.txt
+./target/release/aoc_2021.exe 4 ./resources/day4_input.txt
+./target/release/aoc_2021.exe 5 ./resources/day5_input.txt
+./target/release/aoc_2021.exe 6 ./resources/day6_input.txt
+./target/release/aoc_2021.exe 7 ./resources/day7_input.txt
+./target/release/aoc_2021.exe 8 ./resources/day8_input.txt
+./target/release/aoc_2021.exe 9 ./resources/day9_input.txt
+./target/release/aoc_2021.exe 10 ./resources/day10_input.txt
+./target/release/aoc_2021.exe 11 ./resources/day11_input.txt
+./target/release/aoc_2021.exe 12 ./resources/day12_input.txt
+./target/release/aoc_2021.exe 13 ./resources/day13_input.txt
+./target/release/aoc_2021.exe 14 ./resources/day14_input.txt
+./target/release/aoc_2021.exe 15 ./resources/day15_input.txt
+./target/release/aoc_2021.exe 16 ./resources/day16_input.txt
+./target/release/aoc_2021.exe 17 ./resources/day17_input.txt
+./target/release/aoc_2021.exe 18 ./resources/day18_input.txt
+./target/release/aoc_2021.exe 19 ./resources/day19_input.txt
+./target/release/aoc_2021.exe 20 ./resources/day20_input.txt
+./target/release/aoc_2021.exe 21 ./resources/day21_input.txt
+./target/release/aoc_2021.exe 22 ./resources/day22_input.txt
+# ./target/release/aoc_2021.exe 23 ./resources/day23_input.txt
+# ./target/release/aoc_2021.exe 24 ./resources/day24_input.txt
+# ./target/release/aoc_2021.exe 24 ./resources/day25_input.txt
